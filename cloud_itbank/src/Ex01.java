ec1e31a8db74b3b17709b7bb20da5204b9f8eed7
mageaker <mageaker@NT300E5V>
2017-04-06 00:17:17 +0900

    이클립스 테스트 입니다

---
+++ cloud_itbank/src/Ex01.java
@@ -0,0 +1,23 @@
+
+
+import java.net.*;
+import java.io.*;
+
+public class Ex01 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		DatagramSocket ds = new DatagramSocket();
+		while(true){
+			System.out.println("보낼물건 = ");
+			String msg = br.readLine();
+			for(int i=2;i<254;i++){
+				InetAddress ia = InetAddress.getByName("192.168.0."+i);
+				DatagramPacket dp = new DatagramPacket(
+						msg.getBytes(), msg.getBytes().length, ia, 12345);
+				ds.send(dp);
+			}
+			System.out.println("전송완료");
+		}
+	}
+}
