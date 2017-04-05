29ad1eda6a7fb259131691cf93334ac759b789ac
mageaker <mageaker@NT300E5V>
2017-04-06 00:17:17 +0900

    이클립스 테스트 입니다

---
+++ cloud_itbank/src/Ex02.java
@@ -0,0 +1,17 @@
+
+
+import java.net.*;
+import java.io.*;
+
+public class Ex02 {
+	public static void main(String[] args) throws IOException {
+		DatagramSocket ds = new DatagramSocket(12345);
+		while(true){
+			byte[] data = new byte[65508];
+			DatagramPacket dp = new DatagramPacket(data, data.length);
+			ds.receive(dp);
+			System.out.println(dp.getAddress().getHostAddress()
+					+ " >> "+new String(dp.getData()).trim());
+		}
+	}
+}
