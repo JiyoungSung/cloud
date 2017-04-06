ad4986fee3ba413b8c29ebc3dfcf7ec6d2529780
Administrator <Administrator@samadal-PC>
2017-04-06 16:27:13 +0900

    memberdao수정본

--- cloud_itbank/src/MemberDAO.java
+++ cloud_itbank/src/MemberDAO.java
@@ -3,6 +3,7 @@ package project;
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
@@ -20,30 +21,13 @@ public class MemberDAO {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 		}catch(ClassNotFoundException e){
 			e.printStackTrace();
+			System.out.println("멤버dao 오라클접속 오류");
 		}
 		url="jdbc:oracle:this:@localhost:1521:xe";
 		user="project";
 		pass="project";
 	}
-	public int insertMember(MemberDTO dto){
-		String sql="insert into member vlaues"+"(?,?,?,?,?,?)";
-		int res=0;
-		try{
-			con=DriverManager.getConnection(url,user,pass);
-			ps=con.prepareStatement(sql);
-			ps.setInt(1, dto.getNo());
-			ps.setString(2, dto.getRank());
-			ps.setString(3, dto.getId());
-			ps.setString(4, dto.getPass());
-			ps.setString(5, dto.getEmail());
-			ps.setString(6, dto.getSignday());
-			res=ps.executeUpdate();
-		}catch(SQLException e){
-			e.printStackTrace();
-		}
-		return res;
-	}
-	public void SaveMember(MemberDTO dto) throws IOException{
+	public void SaveMember() throws IOException{
 		File dir2=new File("E:\\jsh_java_workspace\\201704_javaproject\\src\\project");
 		File f2=new File(dir2,"memberdb.txt");
 		String sql="select * from member";
@@ -52,7 +36,9 @@ public class MemberDAO {
 		DataOutputStream dos=new DataOutputStream(bos);
 		try{
 			con=DriverManager.getConnection(url,user,pass);
+			ps=con.prepareStatement(sql);
 			rs=ps.executeQuery();
+     
 			while(rs.next()){//rs.next():hasNext()+next() 의 기능을가지고있다.
 				int no=rs.getInt("no");
 				String rank=rs.getString("rank");
@@ -61,10 +47,10 @@ public class MemberDAO {
 				String signday=rs.getString("signday");
 				dos.writeUTF("넘버:"+no+"등급:"+rank
 						+"아이디:"+id+"email:"+email+"signday:"+signday+"\n");
-			}
-			
+				dos.flush();
+			}	
 		}catch(SQLException e){
-			
+			System.out.println("오류");
 		}
 		
 		/*dos.writeUTF("번호:"+dto.getNo()+"랭크:"+dto.getRank()+"아이디:"+dto.getId()+
