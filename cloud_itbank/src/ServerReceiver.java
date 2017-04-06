2016d48c9f027d9f2b56e3041d03dad31607c092
Administrator <Administrator@samadal-PC>
2017-04-06 21:54:25 +0900

    서버에서 클라가 보내는거 받는것

---
+++ cloud_itbank/src/ServerReceiver.java
@@ -0,0 +1,37 @@
+package project;
+
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+
+public class ServerReceiver {
+
+	public static void main(String[] args) {
+		try {
+			ServerSocket ss = new ServerSocket(12345);
+			Socket s = ss.accept();
+			InputStream is = s.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+			String fileName = br.readLine(); // 클라이언트 에서 파일명을 보내준것을 받음
+			String[] str=fileName.split("%");
+			File f = new File("E:"+File.separator+"jsh_java_workspace"+File.separator+"201704_javaproject"
+					+File.separator+"src"+File.separator+"project"+File.separator+"serverfile"+File.separator+str[0]);
+			if(!f.exists()){//경로가 존재하지않는다면 만든다
+				f.mkdirs();
+			}
+			File f1 = new File(f,str[1]);
+			
+			FileOutputStream out = new FileOutputStream(f1);
+			int i = 0;
+			while ((i = is.read()) != -1) {
+				out.write((char) i);
+			}
+
+			br.close(); is.close(); out.close(); s.close(); ss.close();
+			br = null; is = null; out = null; s = null;	ss = null;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
